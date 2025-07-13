1e21bc7927de6932028f8c01d99fa96e5f20c777
ffrycz <f.frycz.531@studms.ug.edu.pl>
2025-07-13 22:20:15 +0200

    Early version of File Loader

---
+++ src/main/java/org/guessWhoApp/FileLoader.java
@@ -0,0 +1,44 @@
+package org.guessWhoApp;
+
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.FileSystemException;
+import java.util.Arrays;
+
+public class FileLoader {
+    private final JFrame parentFrame;
+
+    public FileLoader(JFrame parentFrame) {
+        this.parentFrame = parentFrame;
+    }
+
+    public String[] loadDirectory() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        try {
+            int returnValue = chooser.showOpenDialog(null);
+
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedDirectory = chooser.getSelectedFile();
+                System.out.println("Selected directory: " + selectedDirectory.getAbsolutePath());
+
+                return getImages(selectedDirectory);
+            }else{
+                throw new FileSystemException("Operation cancelled.");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(parentFrame, "Błąd odczytu: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public String[] getImages(File directory) {
+        String[] imageList = directory.list();
+        if (imageList.length > 24) {
+            return Arrays.stream(imageList).limit(24).toArray(String[]::new);
+        }else{
+            return imageList;
+        }
+    }
+}
